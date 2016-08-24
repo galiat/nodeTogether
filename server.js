75c128c8b170c1502bac400a5dcd20888ee60e00
Galia <galiat@gmail.com>
2016-08-23 21:05:31 -0400

    server connects!

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-require('express');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || '8080';
+const host = process.env.HOST || '0.0.0.0';
+
+// app.use(require('./app'));
+
+app.listen(port, host);
+
+console.log('server running %s:%d', host, port);
+
+// module.exports = app;
