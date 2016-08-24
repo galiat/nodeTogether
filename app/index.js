1f833594c5e21cdaf8d6bc37d2d4b0eceaeaa15c
Galia <galiat@gmail.com>
2016-08-23 21:56:04 -0400

    return all motivations

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
+const motivations = require('motivations');
 const express = require('express');
 const app = express();
 
 app.get("/", function(request, response){
-  response.send("HELLO WORD!");
+  response.send(motivations);
 });
 
 module.exports = app
