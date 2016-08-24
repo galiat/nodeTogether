bb37d62c985d80cfa1990c6daee627b3b5cf1500
Galia <galiat@gmail.com>
2016-08-23 22:49:26 -0400

    use handelbars

--- app/index.js
+++ app/index.js
@@ -1,10 +1,25 @@
 const motivations = require('motivations');
 const pickOne = require('pick-one');
+
 const express = require('express');
+const exphbs  = require('express-handlebars');
+
 const app = express();
 
+app.use(express.static('./app/public'));
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  layoutsDir:'./app/views/layouts'
+}));
+
+app.set('view engine', 'handlebars');
+app.set('views', './app/views');
+
 app.get("/", function(request, response){
-  response.send(pickOne(motivations));
+  var motivation = pickOne(motivations);
+  var image = "";
+  response.render('motivation', { motivation, image });
 });
 
 module.exports = app
