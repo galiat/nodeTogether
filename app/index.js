d575b484881f1a68be19b48544ff2b80a9a4cd13
Galia <galiat@gmail.com>
2016-08-23 21:58:54 -0400

    pick one motivation

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 const motivations = require('motivations');
+const pickOne = require('pick-one');
 const express = require('express');
 const app = express();
 
 app.get("/", function(request, response){
-  response.send(motivations);
+  response.send(pickOne(motivations));
 });
 
 module.exports = app
